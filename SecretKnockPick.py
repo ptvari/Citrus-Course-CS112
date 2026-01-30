3d3cf22e10c871e214005a09f182e2ae9205b0f4
aris <adrian.teniente141@gmail.com>
2026-01-30 15:05:13 -0800

    Create SecretKnockPick.py

---
+++ SecretKnockPick.py
@@ -0,0 +1,9 @@
+"""""
+This is so that we can pick the secret knock for our class
+1/30/26
+Pick at random from a list that we propose
+"""
+import random
+#This is going to be a list, in other languages they call it arrays
+choices = ['No choice']
+print(choices)
