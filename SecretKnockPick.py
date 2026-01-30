be9c8c66e40b124447e858e9607228dc0a42b3bb
aris <adrian.teniente141@gmail.com>
2026-01-30 15:09:53 -0800

    Update SecretKnockPick.py

--- SecretKnockPick.py
+++ SecretKnockPick.py
@@ -5,5 +5,5 @@ Pick at random from a list that we propose
 """
 import random
 #This is going to be a list, in other languages they call it arrays
-choices = ['No choice']
+choices = random.choice(['No choice'])
 print(choices)
