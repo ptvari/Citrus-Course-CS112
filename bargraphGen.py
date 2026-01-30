ae90355d2d1a605d3bba93de10aad8a9c7c184ba
aris <adrian.teniente141@gmail.com>
2026-01-30 14:40:47 -0800

    Added Files
    This is our code as of week 1, buddy.

---
+++ bargraphGen.py
@@ -0,0 +1,10 @@
+"""
+This program is to create bar graph based on questions given to user
+Done in class week 1
+Jan 29, 2026
+"""
+
+questions = ["How much do you like pizza","How much do you like math","How much do you like coding"]
+
+# input from user
+input('Hi buddy! Based on the following questions, give your preferences on scale 1-10.')
---
+++ prog1.py
@@ -0,0 +1,6 @@
+message = 'Hi partner'
+print(message)
+import random
+print('we should do more coding')
+p = random.choice('yn')
+print(p)
