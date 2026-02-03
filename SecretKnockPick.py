7d3e9ba7a3708357be768d402daccc47115ffbe1
aris <adrian.teniente141@gmail.com>
2026-02-03 15:17:16 -0800

    Update SecretKnockPick.py

--- SecretKnockPick.py
+++ SecretKnockPick.py
@@ -8,17 +8,36 @@ import random
 #This is a static list so far buddy hard coded.
 choices = ['tat ta rata +4','one knock 2 coughs','ding ding sneeze']
 #These list will be dynamic
-tat_ta = []
-one_knock = []
-ding = []
-print(choices)
+tat_ta,oneknock,ding= [],[],[]
+print('These are the choices buddy!!!!!!:',choices)
 #sample output
-for i in range(5):
-    print('the pick choice is')
-    pick =random.choice(choices)
-    tat_ta.append(pick)
-print("What got picked is:" "!!!!!!")
-print(tat_ta)
-print(one_knock)
-print(ding)
+for i in range(10):
+    pick=random.choice(choices)
+    #print('the pick choice is:',pick)
+    if pick == choices[0]:
+        tat_ta.append(pick)
+    elif pick == choices[1]:
+        oneknock.append(pick)
+    else:
+        ding.append(pick)
+c=len(tat_ta)
+d=len(oneknock)
+e=len(ding)
+
+for x in tat_ta:
+    print(c,x)
+    c = c +1
+for y in oneknock:
+    print(d,y)
+    d = d +1
+for z in ding:
+    print(e,z)
+    e=e+1
+
+
+"""
+if tat_ta == len()
+    print('This one wins buddy!')
+elif:
 #This does not yet have input
+"""
