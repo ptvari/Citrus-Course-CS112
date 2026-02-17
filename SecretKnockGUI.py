11e07471d76616593933154b31334b7f1951fc03
aris <adrian.teniente141@gmail.com>
2026-02-17 15:19:12 -0800

    Update SecretKnockGUI.py

--- SecretKnockGUI.py
+++ SecretKnockGUI.py
@@ -9,22 +9,30 @@ from SecretKnockPick import choice_buddy_picker
 #tk gui requires a root
 
 root1=tk.Tk()
-
-#place a frame for the input widgets
+root1.geometry("200x100")
+#title of gui
 root1.title("The Offical Secret Nock Picker!")
-inputFrame= tk.Frame(root1,width=200,height=100,bg='light blue')
-title_label= tk.Label(root1, text='The Offical Secret Nock Picker!')
-title_label.pack(pady=10)
+title_label= tk.Label(root1, text='The Offical Secret Nock Picker!', bg='light blue')
+title_label.pack(pady=20)
+
+
+#first frame for entry
+entryFrame= tk.Frame(root1,width=400,height=400,bg='light blue')
+entryFrame.pack()
+entry = tk.Entry(entryFrame, width=25, font=("Times New Roman", 12))
+entry.grid(row=0,column=2)
+
+
+'''
 def on_button_click():
     print("Button clicked! A function was executed.") # This prints to the console
 button = tk.Button(
-    root1,
+    inputFrame,
     text="Add",
     command=on_button_click
 )
-button.pack(pady=50)
-inputFrame2= tk.Frame(root1, bg='light pink', width=200, height=50)
-inputFrame2.pack()
+button.grid()
+'''
 secretknock = choice_buddy_picker(random_items=['tat_ta','ding'],best_outta=11)
 formatOutString = 'The secret knock was {} ' .format(secretknock)
 
