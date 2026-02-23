8ae0c97bf15743eb4d132b5c92e9f223e154af33
aris <adrian.teniente141@gmail.com>
2026-02-23 01:13:56 -0800

    Update SecretKnockGUI.py
    it works!!!

--- SecretKnockGUI.py
+++ SecretKnockGUI.py
@@ -12,15 +12,19 @@ root1=tk.Tk()
 root1.geometry("450x350")
 #title of gui
 root1.title("The Offical Secret Nock Picker!")
-title_label= tk.Label(root1, text='The Offical Secret Nock Picker!', bg='light blue')
-title_label.pack(pady=20)
+title_label= tk.Label(root1, text='The Offical Secret Nock Picker!', bg='light pink')
+title_label.pack(pady=10, padx=10)
 
 
 #first frame for border for entry frame
 borderframe= tk.Frame(root1,padx=40, pady=40, bg="blue")
 borderframe.pack(pady=40)
-title=tk.Label(borderframe, text='Please enter objects to pull from random picker!', pady=5, padx=5, width=35)
+#This changes based on what wins and shows how many times it got picked.
+label_text=tk.StringVar()
+label_text.set("Please enter objects to pull from random picker!")
+title=tk.Label(borderframe, textvariable=label_text, pady=5, padx=5, width=35)
 title.pack()
+
 entry = tk.Entry(borderframe, width=30, font=("Times New Roman", 12))
 entry.pack(pady=10)
 #This will be th global list adder
@@ -29,26 +33,28 @@ def list_adder():
     user_input= entry.get()
     if user_input != "":
         items_list.append(user_input)  # add to list
-        title.config(text="Items: " + ", ".join(items_list))
         print("Option was added")
         print(f"Added: {user_input}")  # show what was added
         print("Full list:", items_list)  # show full list in terminal
+        label_text.set("Items: " + ", ".join(items_list))
         entry.delete(0, tk.END)  # clear the box after adding
 
-
+#This button allows the user to append to the global list
 buttonadd = tk.Button(
     borderframe,
     text="Add",
     command=list_adder
 )
-
+#This runs the picker by using the global list from before
 def run_da_picker():
     if items_list:
         secretknock = choice_buddy_picker(random_items=items_list, best_outta=100)
+        print("Machine is alive!!!")
         formatOutString = 'The secret knock was {} '.format(secretknock)
+        print(formatOutString)
+        label_text.set(formatOutString)
 
-def on_start_click():
-    print("Machine is alive!!!")
+#This button is the start button for the whole random picker, when it presses start, it uses the "run_da_picker" to show what won and how many times.
 buttonstart=tk.Button(
     borderframe,
     text="Start!",
